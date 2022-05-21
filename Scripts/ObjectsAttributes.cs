7e4c5d9b08b67ff2e878108faaba1ac902746683
jiratQ <jirat123@icloud.com>
2022-05-21 15:08:57 +0700

    update health bar

--- Scripts/ObjectsAttributes.cs
+++ Scripts/ObjectsAttributes.cs
@@ -1,25 +1,42 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ObjectsAttributes : MonoBehaviour
 {
-    public int Hp;
-    public int Atk;
-    public int Def;
-    public int Exp;
-    public int Gold;
+    [Header("Stat")]
+    public float MaxHp;
+    private float Hp;
+    public float Atk;
+    public float Def;
+    public float Exp;
+    public float Gold;
 
+    [Header("Health Bar")]
+    public Slider healthBar;
+    public Gradient gradient;
+    public Image fill;
+
+    void Start()
+    {
+        fill.color = gradient.Evaluate(1f);
+        Hp = MaxHp;
+    }
     public void attack(GameObject enermy)
     {
         ObjectsAttributes e = enermy.GetComponent<ObjectsAttributes>();
         e.GetComponent<ObjectsAttributes>().wasAttacked(e.Atk);
     }
-    public void wasAttacked(int damage)
+    public void wasAttacked(float damage)
     {
         Hp -= damage * (100 - Def) / 100;
-        Debug.Log(Hp);
+        SetHealthBar();
         if (Hp <= 0)
         {
             Destroy(gameObject);
         }
+    }
+    private void SetHealthBar()
+    {
+        healthBar.value = Hp / MaxHp;
+        fill.color = gradient.Evaluate(healthBar.normalizedValue);
     }
 }
